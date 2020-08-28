41ecb6244ddaa90f84c80335dcd6d92d329d3385
Igor Guilherme Bianchi <igorgbianchi@gmail.com>
2020-08-28 20:58:56 -0300

    README.me update and docstrings has been added

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # comper-crawler-python
+
+
+## Installation
+To run this script, you'll need ```python >= 3.7``` interpreter installed on your machine. It's recomendable to use an isolated Python environment with ```venv```.
+
+### Linux:
+```sh
+git clone https://github.com/igorgbianchi/comper-crawler-python
+cd comper-crawler-python
+python3 -m venv env
+source env/bin/activate
+pip3 install -r requirements.txt
+python3 app.py
+``` 
+
+See the results on ```output.json```.
+
+### Example
+```json
+[   
+    {
+        "name": "Vinho Chileno Casillero del Diablo Concha y Toro Sauvignon Blanc 750ml",
+        "img_url": "https://comper.vteximg.com.br/arquivos/ids/159755-292-292/56030.jpg?v=637210523685330000",
+        "price": 62.99,
+        "availability": true,
+        "url": "https://www.comperdelivery.com.br/vinho-chileno-casillero-del-diablo-concha-y-toro-s/p"
+    },
+    {
+        "name": "Vodka Destilada Absolut Garrafa 750ml",
+        "img_url": "https://comper.vteximg.com.br/arquivos/ids/178033-292-292/1586025_1.jpg?v=637332718037630000",
+        "price": 84.9,
+        "availability": true,
+        "url": "https://www.comperdelivery.com.br/vodka-absolut-750ml/p"
+    },
+    {
+        "name": "Vinho Paso Grande 750ml Private Reserva Pinot Noir",
+        "img_url": "https://comper.vteximg.com.br/arquivos/ids/177133-292-292/6846.jpg?v=637315393582700000",
+        "price": 0,
+        "availability": false,
+        "url": "https://www.comperdelivery.com.br/vinho-paso-grande-750ml-private-reserva-pinot-noir/p"
+    }
+]
+``` 
--- app.py
+++ app.py
@@ -16,12 +16,29 @@ MS_DIG = 2
 MT_DIG = 1
 
 
-async def fetch(session, url: str):
+async def fetch(session, url: str) -> str:
+    """Fetch an URL async.
+
+    Args:
+        session (aiohttp.ClientSession): An aiohttp Client Session.
+        url (str): URL
+
+    Returns:
+        str: Page text response.
+    """
     async with session.get(url) as response:
         return await response.text()
 
 
-async def fetch_all(urls: list):
+async def fetch_all(urls: list) -> list:
+    """Fetch a list of urls in async mode.
+
+    Args:
+        urls (list): List of URLs
+
+    Returns:
+        list: List of responses as text.
+    """
     async with aiohttp.ClientSession() as session:
         responses = await asyncio.gather(*[
             fetch(session, url)
@@ -30,30 +47,59 @@ async def fetch_all(urls: list):
         return responses
 
 
-if __name__ == '__main__':
-    with open("home.html", 'r') as f:
-        html = str(f.read())
-        f.close()
-    
+def parse_pages(home_page: str) -> list:
+    """According to Comper home-page, it will find 6 product's URL on that.
+
+    Args:
+        home_page (str): Comper home-page to find products.
+
+    Returns:
+        list: List of products URLs.
+    """
+     
     regex = re.compile(ITEMS_REGEX, flags=re.MULTILINE)
-    items = re.findall(regex, html)
+    items = re.findall(regex, home_page)
     item_regex = re.compile(ITEM_URL_REGEX)
     urls = [f"{re.findall(item_regex, item)[0]}?sc={DF_DIG}" for item in items]
     items_response = asyncio.run(fetch_all(urls[:6]))
+    
     items_page_text = [
-        {'page_text': item, 'url': url} 
+        {'text': item, 'url': url} 
         for item, url in zip(items_response, urls) if not re.search(PAGE_ERROR, item)
     ]    
-    data = []    
+    
+    return items_page_text
+
+
+def make_items_data(items_page_text: list) -> list:
+    """Read the page's data layer and get important info to compose a products list.
+
+    Args:
+        items_page_text (list): Products page list as text.
+
+    Returns:
+        list: Products info list.
+    """
+    items_data = []
     
     for item_page in items_page_text:
-        data_layer = json.loads(re.findall(DATA_LAYER_REGEX, item_page['page_text'])[0])
-        data.append({
+        data_layer = json.loads(re.findall(DATA_LAYER_REGEX, item_page['text'])[0])
+        items_data.append({
             "name": data_layer["name"],
             "img_url": data_layer["skus"][0]["image"],
-            "price": data_layer["skus"][0]["bestPrice"]/100,
+            "price": data_layer["skus"][0]["bestPrice"]/100 if data_layer["skus"][0]["available"] else 0,
             "availability": data_layer["skus"][0]["available"],
-            "url": item_page["url"]            
+            "url": item_page["url"].replace(f"?sc={DF_DIG}", "")        
         })
     
-    print(data)
+    return items_data
+
+
+if __name__ == '__main__':
+    home_page = asyncio.run(fetch_all([HOME_URL]))[0]
+    items_page_text = parse_pages(home_page)
+    items_data = make_items_data(items_page_text)
+    
+    with open('output.json', 'w') as f:
+        f.write(json.dumps(items_data[:3]))
+        f.close()
